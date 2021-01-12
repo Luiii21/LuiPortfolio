3b12272e882a6ccbc8b1de130d92d26f257dfb89
Jeanlui21 <pamui_2121@hotmail.com>
2021-01-11 22:16:38 -0500

    landing terminado"

--- src/app/modules/landing/landing-routing.module.ts
+++ src/app/modules/landing/landing-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LandingComponent} from '@components/landing/pages/landing/landing.component';
+import {ProjectsComponent} from '@components/landing/pages/projects/projects.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,10 @@ const routes: Routes = [
       {
         path: '',
         component: LandingComponent
+      },
+      {
+        path: 'projects',
+        component: ProjectsComponent
       }
     ]
   }
--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -4,10 +4,11 @@ import {CommonModule} from '@angular/common';
 import {LandingRoutingModule} from './landing-routing.module';
 import {LandingComponent} from './pages/landing/landing.component';
 import {SharedModule} from '@app/shared/shared.module';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 
 @NgModule({
-  declarations: [LandingComponent],
+  declarations: [LandingComponent, ProjectsComponent],
   imports: [CommonModule, LandingRoutingModule, SharedModule]
 })
 export class LandingModule {
